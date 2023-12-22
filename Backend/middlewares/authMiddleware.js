6b6157c34e1d0c35597a77dbee47c6270bf32fc3
Animesh Sinha <animeshsinha1903@gmail.com>
2023-12-23 01:56:20 +0530

    Fix issues

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const JWT = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.headers["authorization"].split(" ")[1];
+    const token = req.headers["authorization"]?.split(" ")[1];
     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         res.status(401).send({
--- Backend/server.js
+++ Backend/server.js
@@ -26,11 +26,11 @@ app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 
 //Static Folder
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 //Static Routes
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 //port
