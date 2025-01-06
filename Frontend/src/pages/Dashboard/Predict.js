501b05d7829f8376adb1f2488fa7c2a9ad08f2ba
Animesh Sinha <animeshsinha1903@gmail.com>
2025-01-06 15:23:19 +0530

    fixed predict.js

--- Frontend/src/pages/Dashboard/Predict.js
+++ Frontend/src/pages/Dashboard/Predict.js
@@ -238,7 +238,7 @@ const App = () => {
             marginBottom: "16px", 
           }}
         >
-          Total Predicted Demand for the next month: {Math.round(totalMonthly)}ML
+          Total Predicted Demand for the next month: {Math.round(totalMonthly)} ML
         </Box>
       </div>
     </Layout>
